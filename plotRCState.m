43bde1e7053fbc0e288ec7af6b039601d9de96ba
jtylerboylan <jtylerboylan@outlook.com>
2023-09-15 17:29:36 -0400

    ~

--- plotRCState.m
+++ plotRCState.m
@@ -7,8 +7,8 @@ end
 %% CAR INFO
 
 % RC Car properties
-carWidth = 0.45; % m
-carLength = 0.75; % m
+carWidth = 0.1; % m
+carLength = 0.3; % m
 
 % Get current state and control
 position = RC.CurrentState(1:2); % x, y
@@ -16,22 +16,28 @@ theta = RC.CurrentState(3); % theta
 % velocity = (RC.CurrentControl(1) - RC.Options.DriveOffset) / RC.Options.DriveGain;
 gamma = (RC.CurrentControl(2) - RC.Options.SteerOffset) / RC.Options.SteerGain; % gamma
 
+% carWheelbaseLength = 0.3
+% carTrackWidth = 0.2
+% rho = carWheelbaseLength/tan(gamma);
+% gamma_left = atan2(carWheelbaseLength, rho + carTrackWidth/2);
+% gamma_right = atan2(carWheelbaseLength, rho - carTrackWidth/2);
+
 % Car body points
 body_RC = [+carLength/2, -carLength/2, -carLength/2 , +carLength/2;
-    +carWidth/2, +carWidth/2, -carWidth/2, -carWidth/2];
+           +carWidth/2, +carWidth/2, -carWidth/2, -carWidth/2];
 
 
 %% WHEEL INFO
 
 % Wheel properties
-wheelWidth = 0.15; % m
-wheelLength = 0.3; % m
-axleOffsetX = 0.75*(carLength/2); % m
-axleOffsetY = carWidth/2; % m
+wheelWidth = 0.05; % m
+wheelLength = 0.1; % m
+axleOffsetX = 0.85*(carLength/2); % m
+axleOffsetY = (carWidth + wheelWidth)/2; % m
 
 % Wheel body points
 wheel = [+wheelLength/2, -wheelLength/2, -wheelLength/2 , +wheelLength/2;
-    +wheelWidth/2, +wheelWidth/2, -wheelWidth/2, -wheelWidth/2];
+         +wheelWidth/2, +wheelWidth/2, -wheelWidth/2, -wheelWidth/2];
 
 %% SETUP FRAMES
 
@@ -46,7 +52,7 @@ G_trans_RC = position;
 
 % Rotation: Ground frame to RC frame
 G_rot_RC = [cos(theta), -sin(theta);
-    sin(theta), cos(theta)];
+            sin(theta), cos(theta)];
 
 % Translations: Front/back axle to left and right wheel frames
 XW_trans_WL = [0; +axleOffsetY];
@@ -58,7 +64,12 @@ RC_trans_BW = [-axleOffsetX; 0];
 
 % Rotation: RC frame to front axle frame
 RC_rot_FW = [cos(gamma), -sin(gamma);
-    sin(gamma), cos(gamma)];
+             sin(gamma), cos(gamma)];
+
+% RC_rot_FL = [cos(gamma_left), -sin(gamma_left);
+%              sin(gamma_left), cos(gamma_left)];
+% RC_rot_FR = [cos(gamma_right), -sin(gamma_right);
+%              sin(gamma_right), cos(gamma_right)];
 
 %% APPLY TRANSFORMS
 
@@ -66,10 +77,10 @@ RC_rot_FW = [cos(gamma), -sin(gamma);
 body_G = G_trans_RC + G_rot_RC*body_RC;
 
 % Wheels transformations
-wheelFL_G = G_trans_RC + G_rot_RC*(RC_trans_FW + RC_rot_FW*(wheel + XW_trans_WL)); % front left
+wheelFL_G = G_trans_RC + G_rot_RC*(RC_trans_FW + (RC_rot_FW*wheel + XW_trans_WL)); % front left
 wheelBL_G = G_trans_RC + G_rot_RC*(RC_trans_BW + (wheel + XW_trans_WL)); % back left
 wheelBR_G = G_trans_RC + G_rot_RC*(RC_trans_BW + (wheel + XW_trans_WR)); % back right
-wheelFR_G = G_trans_RC + G_rot_RC*(RC_trans_FW + RC_rot_FW*(wheel + XW_trans_WR)); % front right
+wheelFR_G = G_trans_RC + G_rot_RC*(RC_trans_FW + (RC_rot_FW*wheel + XW_trans_WR)); % front right
 
 %% PLOT
 
@@ -90,7 +101,7 @@ else
     set(display(5), 'xdata', wheelFR_G(1,:), 'ydata', wheelFR_G(2,:))
 end
 
-halfWindowSize = 1.5;
+halfWindowSize = 0.5;
 axis([G_trans_RC(1) - halfWindowSize, ...
     G_trans_RC(1) + halfWindowSize, ...
     G_trans_RC(2) - halfWindowSize, ...
