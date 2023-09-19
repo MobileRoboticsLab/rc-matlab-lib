27b97541c164dbfc263d326478db26e91137e8e4
jtylerboylan <jtylerboylan@outlook.com>
2023-09-19 17:05:01 -0400

    Updates

--- control/RCControl.m
+++ control/RCControl.m
@@ -4,24 +4,17 @@ classdef RCControl < handle
         RC
 
         ControlTimer
-        PlannerTimer
-
         ControlFcn
-        PlannerFcn
-
         ControlFreq
-        PlannerFreq
 
         isRunning
     end
 
     methods
-        function obj = RCControl(RC, controlHandle, plannerHandle)
+        function obj = RCControl(RC, controlFcn, controlFreq)
             obj.RC = RC;
-            obj.ControlFreq = 10;
-            obj.ControlFcn = controlHandle;
-            obj.PlannerFreq = 0.5;
-            obj.PlannerFcn = plannerHandle;
+            obj.ControlFreq = controlFreq;
+            obj.ControlFcn = controlFcn;
             obj.isRunning = false;
         end
 
@@ -29,14 +22,6 @@ classdef RCControl < handle
             if ~obj.isRunning
 
                 obj.RC.clearLogs();
-
-                fprintf("Starting RC Planner @ %.1f Hz. \n", obj.PlannerFreq)
-
-                % planner timer
-                obj.PlannerTimer = timer('Period', 1/obj.PlannerFreq, ...
-                    'ExecutionMode', 'fixedRate', ...
-                    'TimerFcn', @(~,~) obj.PlannerFcn(obj.RC));
-                start(obj.PlannerTimer);
 
                 fprintf("Starting RC Control @ %.1f Hz. \n", obj.ControlFreq)
 
@@ -54,10 +39,6 @@ classdef RCControl < handle
 
         function stop(obj)
             if obj.isRunning
-
-                stop(obj.PlannerTimer)
-                delete(obj.PlannerTimer)
-                obj.PlannerTimer = [];
 
                 stop(obj.ControlTimer)
                 delete(obj.ControlTimer)
