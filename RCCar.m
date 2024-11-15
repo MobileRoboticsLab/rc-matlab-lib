068ff687a99c7d68fe0a1ccc60bbf52ee8a2256e
Jonathan Tyler Boylan <jtylerboylan@outlook.com>
2024-11-15 08:12:29 -0500

    Add random number to name to prevent duplicate node issue

--- RCCar.m
+++ RCCar.m
@@ -80,7 +80,7 @@ classdef RCCar < handle
             obj.MaxSteerAngle = +0.25; % rad
             obj.MinSteerAngle = -0.25; % rad
 
-            obj.Node = ros.Node('rc_car_matlab_node');
+            obj.Node = ros.Node(strcat('rc_car_matlab_node_', int2str(randi(10))));
 
             % Subscribers
             obj.PoseSubscriber = ros.Subscriber(obj.Node, ...
